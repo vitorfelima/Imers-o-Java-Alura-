5a1837bbdf8a1c55b2719bbba0aa2f84a1b79168
vitorfelima <vitorfelima@gmail.com>
2022-07-20 17:47:12 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
+		URI endereco = URI.create(url);
+		var client = HttpClient.newHttpClient();
+		var request = HttpRequest.newBuilder(endereco).GET().build();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		String body = response.body();
+		
+		
+		var parser = new JsonParser();
+		List<Map<String, String>> listaDeFilmes = parser.parse(body);
+		
+		for (Map<String, String> filme : listaDeFilmes) {
+			
+			System.out.println(filme.get("title"));
+			System.out.println(filme.get("image"));
+			System.out.println(filme.get("imDbRating"));
+			System.out.println();
+			
+		}
+	}
+
+}
